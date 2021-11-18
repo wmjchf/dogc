379482625385c53d6b5e2e95cf94793a0723fb11
wmjchf <3284304056@qq.com>
2021-11-18 23:58:03 +0800

    fix: 文案修改

--- example/src/pages/Home/index.tsx
+++ example/src/pages/Home/index.tsx
@@ -4,14 +4,18 @@
  * @Author: wjm
  * @Date: 2021-11-06 15:08:49
  * @LastEditors: sueRimn
- * @LastEditTime: 2021-11-06 19:30:13
+ * @LastEditTime: 2021-11-18 23:40:56
  */
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { Button } from "@bufang/dogc";
 import "./style/index.less";
 
 const Home: React.FC = () => {
-  return <div className="home">react移动端组件库</div>;
+  return (
+    <div className="home">
+      <Button type="123">按钮</Button>
+    </div>
+  );
 };
 
 export default Home;
